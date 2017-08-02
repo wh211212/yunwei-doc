292a2a34c7ef55dea4ebf4e17ecc3c511d174471
wanghui <hwang@aniu.tv>
2017-08-02 08:40:07 +0800

    add deploy scripts

--- shell-doc/deploy-shell/deploy-aniuapi.sh
+++ shell-doc/deploy-shell/deploy-aniuapi.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+#---------------------------------------
+# Functions: deploy java project 
+# Changelog:
+# 2017-08-01 wanghui initial
+#---------------------------------------
+# define variables
+Workspace=/var/lib/jenkins/workspace
+Project_home=old-project
+
+# mve test project 
+export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.141.x86_64
+
+java -version
+
+
