3d6bb1ac18535e9fe4fbe2e2fc02282a49f3a902
wanghui <hwang@aniu.tv>
2018-06-05 10:05:15 +0800

    add somefile

---
+++ shell/delete-backwar-oldlogs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# ***********************************************
+# @Time     : 2018/4/26 18:29
+# @Author   : shaonbean@qq.com
+# @Software : PyCharm
+# @Blog     : http://blog.csdn.net/wh211212
+# ***********************************************
