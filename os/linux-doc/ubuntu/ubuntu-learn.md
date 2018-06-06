46820ac29fb93acf9baa6a469fdd10dd4bce78a3
wanghui <hwang@aniu.tv>
2018-06-06 09:29:41 +0800

    add

---
+++ os/linux-doc/ubuntu/ubuntu-learn.md
@@ -0,0 +1,12 @@
+# ubuntu 学习
+
+- 查看ubuntu版本
+
+cat /etc/issue
+
+sudo lsb_release -a
+
+- 防火墙设置
+ufw enable
+ufw disable
+ufw status
--- shell/delete-backwar-oldlogs.sh
+++ shell/delete-backwar-oldlogs.sh
@@ -5,3 +5,20 @@
 # @Software : PyCharm
 # @Blog     : http://blog.csdn.net/wh211212
 # ***********************************************
+#
+warback_dir=/data/warback/aniu-api
+find /data/warback/aniu-api/* -mtime +15 -exec rm -rf {} \;
+
+# define port list array
+portlist=(7081 7082 7083)
+
+# define functions
+function deploy() {
+
+for port in ${portlist[@]};
+  do
+  tomcat_port=tomcat-$port
+  tomcat_home=/data/tomcats/${tomcat_port}
+  [ -d ${tomcat_home} ] && find ${tomcat_home}/logs/* -mtime exec -rm -rf {} \;
+}
+
